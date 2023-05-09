18752ffe9767b9d782504579963b59fd29df3255
Joel Southall <southallj@my.lanecc.edu>
2023-05-09 01:42:53 -0700

    create class Eliza and main driver to test it.

---
+++ Eliza.cs
@@ -0,0 +1,26 @@
+﻿namespace ElizaClasses
+{
+    public class Eliza
+    {
+        public string CreateElizaResponse(string clientStatement)
+        {
+            if (clientStatement.Contains("my"))
+            {
+                // extract the noun after "my" from the client statement
+                string[] words = clientStatement.Split(' ');
+                int index = Array.IndexOf(words, "my");
+                if (index >= 0 && index < words.Length - 1)
+                {
+                    string noun = words[index + 1];
+                    return $"Tell me more about your {noun}.";
+                }
+            }
+            else if (clientStatement.Contains("love") || clientStatement.Contains("hate"))
+            {
+                return "You have strong feelings about that!";
+            }
+
+            return "Please go on.";
+        }
+    }
+}
---
+++ program.cs
@@ -0,0 +1,27 @@
+﻿using ElizaClasses;
+using System;
+
+namespace ElizaDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Eliza eliza = new Eliza();
+            Console.WriteLine("Talk to your robot therapist, Eliza: ");
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                string response = eliza.CreateElizaResponse(input);
+                Console.WriteLine(response);
+            }
+        }
+    }
+}
